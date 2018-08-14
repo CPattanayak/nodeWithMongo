27f74660f5e947a3538b51312a4034b68b7ce97e
Prashant Kumar <prashtechno@gmail.com>
2018-08-14 12:30:53 +0530

    manifest xml

--- express-swagger/db/mongoose.js
+++ express-swagger/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://test:test1234@ds018538.mlab.com:18538/mongodemo');
 
 module.exports = {mongoose};
