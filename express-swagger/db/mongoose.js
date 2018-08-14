52bb98f227edb0af6dead4573f3f3e031ee1a7ce
Pattanayak <Chandan.Pattanayak@emc.com>
2018-08-14 12:40:25 +0530

    added environment variable

--- express-swagger/db/mongoose.js
+++ express-swagger/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+var mongoUrl= process.env.MONGO_URL || 'mongodb://localhost:27017/TodoApp'
+
 mongoose.connect('mongodb://test:test1234@ds018538.mlab.com:18538/mongodemo');
 
 module.exports = {mongoose};
